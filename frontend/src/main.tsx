42faadef2b0a688f9eec8781e124e416fe113a2a
Kiryl Tserpilouski <terpilovskikirill@gmail.com>
2024-01-31 19:11:13 +0100

    added bootstrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.scss";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/root";
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
