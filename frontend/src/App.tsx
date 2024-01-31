293147150f47dbb0232fdda65808b2bb2c297386
Марк <lolka-pop@rambler.ru>
2024-01-31 22:47:39 +0300

    feat: add Container for Outlet in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,32 @@
-import { Link, Outlet } from "react-router-dom";
+import { Container } from 'react-bootstrap';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to={"account"}>Account</Link>
-            </li>
-            <li>
-              <Link to={"home"}>Home</Link>
-            </li>
-            <li>
-              <Link to={"today"}>Today</Link>
-            </li>
-            <li>
-              <Link to={"my-riceps"}>My riceps</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div>
-        <Outlet />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to={'account'}>Account</Link>
+						</li>
+						<li>
+							<Link to={'home'}>Home</Link>
+						</li>
+						<li>
+							<Link to={'today'}>Today</Link>
+						</li>
+						<li>
+							<Link to={'my-riceps'}>My riceps</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<Container>
+				<Outlet />
+			</Container>
+		</>
+	);
 }
 
 export default App;
