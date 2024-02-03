76bb0bd02add65e39aee9a06e3b885e2a1c2b3db
Марк <lolka-pop@rambler.ru>
2024-02-04 00:35:32 +0300

    dev: add redux

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,14 +5,15 @@ import searchEdamamAPI from '../utils/searchEdamamAPI.js';
 //@route GET /api/products
 //@access Public
 export const searchProductApi = asyncHandler(async (req, res) => {
-	const query = req.body.query;
+	const query = req.params.query;
+  console.log(req.params)
 	try {
 		const apiRes = await searchEdamamAPI(query);
+    console.log(apiRes)
     const result = apiRes.hints || []
 		res.status(200).json(result);
 	} catch (error) {
 		res.status(404);
 		throw new Error(error);
 	}
-
 });
